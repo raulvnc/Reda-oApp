3d8ec89e68f1eda13fc66058b57208c406466dc1
Raul Vila Nova Costa <rvnc@cesar.school>
2026-05-06 22:18:28 -0300

    Initial Commit

---
+++ Redaçãoapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Redaçãoapp
+//
+//  Created by Raul Vila nova costa on 06/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Redaçãoapp/Redac_a_oappApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redac_a_oappApp.swift
+//  Redaçãoapp
+//
+//  Created by Raul Vila nova costa on 06/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Redac_a_oappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
